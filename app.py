56e30c382943edc0cefb24f042359f05d9f99d94
avac886 <avac886@gmail.com>
2025-05-09 09:14:18 -0400

    poopoo

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    response = requests.get("https://hp-api.onrender.com/api/characters")
+    data = response.json()
+    harry_potter_list = data['results']
+    characters = []
+
+    for character in harry_potter_list:
+        url = character['url']
+        parts = url.strip("/").split("/")
+        id = parts[-1]
