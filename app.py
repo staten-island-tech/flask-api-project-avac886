25c27853d698e254c95836dfb72b4386b630c9a8
avac886 <avac886@gmail.com>
2025-05-12 17:54:42 -0400

    e

--- app.py
+++ app.py
@@ -14,9 +14,12 @@ hpHouses = [
 def home():
     return render_template("index.html", hpHouses=hpHouses)
 
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 """ @app.route("/house:house") """
-
-
 
 """ @app.route("/gryffindor")
 def gryffindor():
