f0aee702dbd785d41751ba7de3470b7f5aeb2a4d
avac886 <avac886@gmail.com>
2025-05-15 09:24:38 -0400

    eeee

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import requests
 app = Flask(__name__)
 
 hpHouses = [
-    {"id": 1, "house": "Gryffindor", "description": "Gryffindor students are known for their bravery, courage, and determination, with a strong sense of justice and a love for the underdog. They are often described as daring, passionate, and quick-thinking, willing to take risks and stand up for what they believe in."},
-    {"id": 2, "house": "Hufflepuff", "description": "Hufflepuffs in the Harry Potter universe are known for their strong sense of loyalty, hard work, dedication, fairness, and a belief in teamwork. They value these traits above all else and are often characterized as humble, kind, and patient. Hufflepuffs are also known for their dedication to what they do and their focus on doing things properly."},
-    {"id": 3, "house": "Ravenclaw", "description": "Ravenclaws are known for their intelligence, wisdom, wit, and creativity. They value learning and intellectual pursuits, and often possess a unique perspective and are open-minded."},
-    {"id": 4, "house": "Slytherin", "description": "Slytherin traits include ambition, cunning, leadership, resourcefulness, determination, and a strong sense of self-preservation. They are also known for their intelligence, shrewdness, and a tendency to prioritize their own goals and well-being. "}
+    {"id": 1, "house": "Gryffindor", "description": "Gryffindor students are known for their bravery, courage, and determination, with a strong sense of justice and a love for the underdog. They are often described as daring, passionate, and quick-thinking, willing to take risks and stand up for what they believe in.", "picture": ""},
+    {"id": 2, "house": "Hufflepuff", "description": "Hufflepuffs in the Harry Potter universe are known for their strong sense of loyalty, hard work, dedication, fairness, and a belief in teamwork. They value these traits above all else and are often characterized as humble, kind, and patient. Hufflepuffs are also known for their dedication to what they do and their focus on doing things properly.", "picture": ""},
+    {"id": 3, "house": "Ravenclaw", "description": "Ravenclaws are known for their intelligence, wisdom, wit, and creativity. They value learning and intellectual pursuits, and often possess a unique perspective and are open-minded.", "picture": ""},
+    {"id": 4, "house": "Slytherin", "description": "Slytherin traits include ambition, cunning, leadership, resourcefulness, determination, and a strong sense of self-preservation. They are also known for their intelligence, shrewdness, and a tendency to prioritize their own goals and well-being.", "picture": ""}
 ]
 
 @app.route("/")
